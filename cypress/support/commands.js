6a45911a9439628bc190a042b6ec45333cb97c16
edgewords <info@edgewords.co.uk>
2021-02-11 15:41:36 +0000

    fixed a but in commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,21 +5,21 @@
 import "cypress-audit/commands";
 
 //Example of our own custom command with some custom logging
-Cypress.Commands.add("login", function(username, password) {
-    //override the default logging with our own log object
-    //Use 'Segoe UI Emoji' font for the padlock icon!
-    const log = Cypress.log({
-        name: "login",
-        displayName: "LOGIN",
-        message: [`🔐 Authenticating user: ${username}`]
-      });
+Cypress.Commands.add("login", function (username, password) {
+  //override the default logging with our own log object
+  //Use 'Segoe UI Emoji' font for the padlock icon!
+  const log = Cypress.log({
+    name: "login",
+    displayName: "LOGIN",
+    message: [`🔐 Authenticating user: ${username}`]
+  });
 
-    cy.visit('https://www.edgewordstraining.co.uk/webdriver2/');
-    cy.contains('Login To Restricted Area').click();
-    log.snapshot("before"); //Take a DOM snapshot. This is the before, we can also specify the after
-    cy.get('#username').type(username);
-    cy.get('#password').type(password);
-    cy.contains('Submit').click();
-    cy.wait (1000, {log: false}); // example of telling cypress not to log this command
-    log.snapshot("before"); //Take another DOM snapshot, the 'after' snapshot
-    });
+  cy.visit('https://www.edgewordstraining.co.uk/webdriver2/');
+  cy.contains('Login To Restricted Area').click();
+  log.snapshot("before"); //Take a DOM snapshot. This is the before, we can also specify the after
+  cy.get('#username').type(username);
+  cy.get('#password').type(password);
+  cy.contains('Submit').click();
+  cy.wait(1000, { log: false }); // example of telling cypress not to log this command
+  log.snapshot("after"); //Take another DOM snapshot, the 'after' snapshot
+});
